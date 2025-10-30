da1df93d80b6e9a619ba237d46524930090293e2
amiralibaleghi <amiralibaleghii@gmail.com>
2025-10-30 19:23:42 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import telebot
 import os
-# توکن رباتت که از BotFather گرفتی
+
 TOKEN = os.getenv("TOKEN")
 
 bot = telebot.TeleBot(TOKEN)
 
-# وقتی کاربر هر پیامی فرستاد
+
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, f"پیامت رو گرفتم ✅\nمتنت: {message.text}")
